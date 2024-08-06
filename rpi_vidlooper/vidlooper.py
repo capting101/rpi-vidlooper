dbc519133d445b5c29742477e48d0262cb3f03e2
capting101 <jeffahalleck@gmail.com>
2024-08-06 03:43:19 +0000

    modified:   rpi_vidlooper/vidlooper.py

--- rpi_vidlooper/vidlooper.py
+++ rpi_vidlooper/vidlooper.py
@@ -131,11 +131,11 @@ class VidLooper(object):
                 # Start a new video player process, capture STDOUT to keep the
                 # screen clear. Set a session ID (os.setsid) to allow us to kill
                 # the whole video player process tree.
-                cmd = ['vlc', '-f', '--video-on-top', "--intf display"]
+                cmd = ['cvlc', '-f']
                 if self.loop:
                     cmd += ['-L']
                 if self.no_osd:
-                    cmd += ['--no-video-title-show']
+                    cmd += ['--no-osd']
                 self._p = Popen(cmd + [filename],
                                 stdout=None if self.debug else PIPE,
                                 preexec_fn=os.setsid)
